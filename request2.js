d224fc08a9def15f80c0e7c179b87d35a1067f35
rsrawley <rsrawley@gmail.com>
2021-10-29 23:03:02 -0400

    Now sets user agent in request header

--- request2.js
+++ request2.js
@@ -15,8 +15,14 @@ module.exports = function (options,callback) {
 		protocol = require('http');
 	}
 
-
-	protocol.get(url, function (resp) {
+	// Set user agent header
+	const protocolOptions ={
+		headers : {
+			"user-agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Safari/537.36"
+		}
+	}
+	
+	protocol.get(url, protocolOptions, function (resp) {
 	  let body = "";
 	  let json;
 
